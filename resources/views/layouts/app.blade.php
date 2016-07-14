2f1fb29930624706ca69081ddb7d534b2ad55fe8
Leo Luchini <leonel.luchini@ec.gba.gov.ar>
2016-07-14 10:26:33 -0300

    nuevos efectos visuales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {!! Html::style('css/carousel.css') !!}
+    {!! Html::style('css/scrolling-nav.css') !!}
     {!! Html::style('css/app.css') !!}
 
     <style>
@@ -26,23 +27,44 @@
         }
     </style>
 </head>
-<body id="app-layout">
-    <div class="navbar-wrapper">
-      <div class="container">
-
-        <nav class="navbar navbar-inverse navbar-inverse-violet navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="{{ url('/') }}">PM</a>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">PM</a>
             </div>
-            <div id="navbar" class="navbar-collapse collapse">
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav navbar-right">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a class="page-scroll" href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#about">Quienes Somos</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#statistics">Estadística</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#services">Servicios</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#staff">Staff</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#clients">Nuestros Clientes</a>
+                        </li>
+                    </ul>
                     @if(Auth::guest())
                         <li class="dropdown{{ $errors->has('email') ? ' open' : '' }}">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Iniciar Sesi&oacute;n</b> <span class="caret"></span></a>
@@ -67,13 +89,10 @@
                     @endif
                 </ul>
             </div>
-          </div>
-        </nav>
-
-      </div>
-    </div>
-
-
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
     <!-- Carousel
     ================================================== -->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -123,12 +142,18 @@
         </div>
       </div>
     </div><!-- /.carousel -->
-    <div class="container-fluid">
-        @yield('content')
-    </div>
+    @yield('content')
+
     <!-- JavaScripts -->
+
+    <!-- jQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    
+    <!-- Bootstrap Core JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-</body>
+    
+    <!-- Scrolling Nav JavaScript -->
+    {!! Html::script('js/jquery.easing.min.js') !!}
+    {!! Html::script('js/scrolling-nav.js') !!}
+  </body>
 </html>
